407f749df70309cb2af5fbc92ead3b099b698a43
GermanCoding <Nummer378@aknm-craft.de>
2016-06-05 01:21:59 +0200

    Add license and license headers to all files

--- src/com/germancoding/taleofpecora/ConfigStorage.java
+++ src/com/germancoding/taleofpecora/ConfigStorage.java
@@ -1,3 +1,8 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Maximilian Froehling aka GermanCoding.
+ * All rights reserved. This program is made available under the terms of the Reciprocal Public License 1.5 
+ * which accompanies this distribution, and is available at https://opensource.org/licenses/RPL-1.5
+ *******************************************************************************/
 package com.germancoding.taleofpecora;
 
 import java.io.File;
